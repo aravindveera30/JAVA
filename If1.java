58f786670bb5e04fe043833408c42ac58a8d75d6
Aravind v <50728054+aravindveera30@users.noreply.github.com>
2019-05-27 11:42:53 +0530

    Add files via upload

---
+++ If1.java
@@ -0,0 +1,14 @@
+class If1{
+public static void main(String arg[]){
+char c='t';
+if(c<=64){
+System.out.println("number");
+}else 
+if(c<='Z')
+{
+System.out.println("upper");
+}
+else
+{
+System.out.println("lower");
+}}}
