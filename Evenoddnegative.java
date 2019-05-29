d019e44571e8b8f874f2a18bdedeb0ae1a09d35a
Aravind v <50728054+aravindveera30@users.noreply.github.com>
2019-05-29 20:51:25 +0530

    Add files via upload

---
+++ Evenoddnegative.java
@@ -0,0 +1,20 @@
+class Evenoddnegative
+{
+public static void main(String arg[])
+{
+int a=-11;
+if(a<0)
+{
+System.out.println("negative number");
+}else 
+if((a%2)==0)
+{
+System.out.println("even no.");
+}else
+if ((a%3)==0)
+{
+System.out.println("odd no.");
+}else
+{
+System.out.println("not");
+}}}
